8e96672c33b7f3d11c676fe51d204bf0eb24b461
Mustafa Yılmaz <mstfylmz10@outlook.com>
2023-04-02 02:30:26 +0300

    Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -7,14 +7,11 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
             //int, decimal, float, double, bool = değer tip
             //array, class, interface = referans tip
             //bellekte stack ve heap diye iki tane alan var
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
 
         }
     }
