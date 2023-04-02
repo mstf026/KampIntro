d4e55976b03e210160754993a2fe9e87e3fc136d
Mustafa Yılmaz <mstfylmz10@outlook.com>
2023-04-02 19:00:47 +0300

    4. ders sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[]
+            //{   "Engin",
+            //    "Gökhan",
+            //    "İsmail",
+            //    "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "Mustafa";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string>
+            {
+                "Engin",
+                "Gökhan",
+                "İsmail",
+                "Halil"
+            };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Mustafa");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
